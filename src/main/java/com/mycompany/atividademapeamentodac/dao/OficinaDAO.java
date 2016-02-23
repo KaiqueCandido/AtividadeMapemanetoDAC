548bdbfa0c9ca9d3956d5b24f5b2d5da0ef826d9
Kaique Candido <kaiquecandidodeoliveira@gmail.com>
2016-02-23 08:07:28 -0300

    Criação da atividade

---
+++ src/main/java/com/mycompany/atividademapeamentodac/dao/OficinaDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.atividademapeamentodac.dao;
+
+/**
+ *
+ * @author kaique
+ */
+public class OficinaDAO {
+    
+}
---
+++ src/main/java/com/mycompany/atividademapeamentodac/entidade/Funcionario.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.atividademapeamentodac.entidade;
+
+import com.mycompany.atividademapeamentodac.enun.Funcao;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kaique
+ */
+@Entity
+public class Funcionario {
+
+    @Id
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private float salario;
+    private Funcao funcao;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(Integer id, String nome, String cpf, String rg, float salario, Funcao funcao) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public Funcao getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(Funcao funcao) {
+        this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", salario=" + salario + ", funcao=" + funcao + '}';
+    }
+
+}
